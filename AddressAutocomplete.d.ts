b3ea914fa30db425873c2b8438e638427daf3293
Zenoo <perso@franckdemoute.fr>
2021-12-09 23:27:36 +0100

    feat: Simplify further

---
+++ AddressAutocomplete.d.ts
@@ -0,0 +1,13 @@
+export default AddressAutocomplete;
+/**
+ * AddressAutocomplete component
+ * @param {Object} props
+ * @param {String} props.apiKey Google Maps API key
+ * @param {String} props.label  Label for the input
+ * @returns {React.ReactElement}
+ */
+declare function AddressAutocomplete({ apiKey, label, ...rest }: {
+    apiKey: string;
+    label: string;
+}): React.ReactElement;
+//# sourceMappingURL=AddressAutocomplete.d.ts.map
