d50344ac819f5f15127d5a70a29d0e32d10ce332
Zenoo <perso@franckdemoute.fr>
2021-12-09 15:22:47 +0100

    fix: Simplify source

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,22 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import sass from 'rollup-plugin-sass'
+import typescript from 'rollup-plugin-typescript2'
 
-const packageJson = require("./package.json");
+import pkg from './package.json'
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-    ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
-];
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    }
+  ],
+  plugins: [
+    sass({ insert: true }),
+    typescript({ objectHashIgnoreUnknownHack: true })
+  ],
+  external: ['react', 'react-dom']
+}
