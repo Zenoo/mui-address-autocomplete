cb60d9f8c5c5cfadda28fcb42c02cf1ef71199d9
Zenoo <perso@franckdemoute.fr>
2021-12-10 13:19:06 +0100

    fix: Wrong TS props

--- dist/AddressAutocomplete.d.ts
+++ dist/AddressAutocomplete.d.ts
@@ -1,17 +1,8 @@
-export interface BoxTypeMap<P = {}> {
-  props: P & {
-    apiKey: string;
-    label: string;
-  };
+export interface AddressAutocompleteProps {
+  apiKey: string;
+  label: string;
 }
 
-/**
- * AddressAutocomplete component
- * @param {Object} props
- * @param {String} props.apiKey Google Maps API key
- * @param {String} props.label  Label for the input
- * @returns {React.ReactElement}
- */
-declare const AddressAutocomplete: React.ReactElement<BoxTypeMap>;
+declare const AddressAutocomplete: (props: AddressAutocompleteProps) => JSX.Element;
 
 export default AddressAutocomplete;
