9557196cb734400dba89c96f6d40ee08ddd97567
Zenoo <perso@franckdemoute.fr>
2021-12-27 10:08:08 +0100

    fix: Add missing callbacks on deselect

--- src/AddressAutocomplete.js
+++ src/AddressAutocomplete.js
@@ -69,6 +69,9 @@ const AddressAutocomplete = ({
         setAddressValue(placeWithComponents);
         onChange(placeWithComponents);
       });
+    } else {
+      setAddressValue(null);
+      onChange(null);
     }
   }, [onChange]);
 
